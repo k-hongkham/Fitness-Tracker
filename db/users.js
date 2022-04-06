f6b5c405f4e46b7ebb893565cb1311d7d5d27961
pam <take_asi@hotmail.com>
2022-04-06 11:12:23 -0500

    created function getUserByUsername

--- db/users.js
+++ db/users.js
@@ -59,8 +59,25 @@ async function getUserById(id) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT * FROM users,
+      WHERE username = $1;
+      `,[username]
+    )
+    return user
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   createUser,
   getUser,
   getUserById,
+  getUserByUsername,
 };
