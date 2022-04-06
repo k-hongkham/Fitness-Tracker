02bb08d37a325e9b862ab0529b3e770605fffbef
k-hongkham <k.hongkham@gmail.com>
2022-04-06 13:52:42 -0400

    createActivity function added.

--- db/activities.js
+++ db/activities.js
@@ -39,7 +39,14 @@ async function createActivity({ name, description }) {
   try {
     const {
       rows: [activity],
-    } = await client.query();
+    } = await client.query(
+      `
+        INSERT INTO activities (name, description)
+        VALUES($1, $2)
+        RETURNING *;
+        `,
+      [name, description]
+    );
     return activity;
   } catch (error) {
     throw error;
@@ -49,4 +56,5 @@ async function createActivity({ name, description }) {
 module.exports = {
   getActivityById,
   getActivities,
+  createActivity,
 };
