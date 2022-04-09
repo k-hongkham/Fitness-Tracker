70a4bdf7fd605b4f4a2dbcba50937a23a35de60d
k-hongkham <k.hongkham@gmail.com>
2022-04-09 15:52:44 -0400

    set up api server.

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 // create the express server here
 const PORT = 3000;
-
 const express = require("express");
 const app = express();
+const apiRouter = require("./api");
+const morgan = require("morgan");
+const cors = require("cors");
+const client = require("./db/client");
+
+app.use(cors());
+app.use(morgan("dev"));
+require("dotenv").config();
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+
+  next();
+});
+
+// causes server to crash
+// app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
