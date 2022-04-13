bfaa657e5656012d29ca50ca182974bbb47cea43
pam <take_asi@hotmail.com>
2022-04-13 15:26:19 -0500

    help test passed

--- api/index.js
+++ api/index.js
@@ -1,3 +1,14 @@
 // create an api router
 // attach other routers from files in this api directory (users, activities...)
 // export the api router
+const express = require ("express");
+const apiRouter = express.Router();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+
+
+apiRouter.get("/health", (req, res, next) => {
+    res.send({ message: "Working" });
+  });
+
+  module.exports = apiRouter
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // create the express server here
+require("dotenv").config();
 const PORT = 3000;
 const express = require("express");
 const app = express();
@@ -6,10 +7,10 @@ const apiRouter = require("./api");
 const morgan = require("morgan");
 const cors = require("cors");
 const client = require("./db/client");
+client.connect();
 
 app.use(cors());
 app.use(morgan("dev"));
-require("dotenv").config();
 app.use(express.json());
 app.use((req, res, next) => {
   console.log("<____Body Logger START____>");
@@ -20,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 // causes server to crash
-// app.use("/api", apiRouter);
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
