519468bd6188dbce3a762ecffa102841c456e2c8
Pam <take_asi@hotmail.com>
2022-04-17 12:53:04 -0500

    utils

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // create the express server here
+require("dotenv").config();
 const PORT = 3000;
 const express = require("express");
 const app = express();
 const apiRouter = require("./api");
 const morgan = require("morgan");
 const cors = require("cors");
-const client = require("./db/client");
+// const client = require("./db/client");
 
 app.use(cors());
 app.use(morgan("dev"));
-require("dotenv").config();
+// require("dotenv").config();
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -23,9 +24,14 @@ app.use((req, res, next) => {
 // causes server to crash
 // app.use("/api", apiRouter);
 app.use("/api", apiRouter);
-app.get("/api", function (req,res, next) {
-  res.json({msg: "This is Cors-enable for all"})
-})
+
+const client = require("./db/client");
+client.connect();
+
+
+// app.get("/api", function (req,res, next) {
+//   res.json({msg: "This is Cors-enable for all"})
+// })
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
